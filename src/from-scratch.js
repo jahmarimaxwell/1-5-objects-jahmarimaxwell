dee5025f940bb02e3ee2afcd26862d25b4b71660
Mari300 <jahfly9@gmail.com>
2024-04-11 14:30:43 -0400

    first function completed

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,5 +1,9 @@
-const coolGreeting = () => {
-};
+const coolGreeting = (person) => {
+  if (person.isCool)
+    return `What is UP ${person.name.toUpperCase()}? How you been doin'?`
+  else
+    return `Greetings ${person.name}, how have you been lately?`
+}
 
 const haveBirthday = () => {
 };
